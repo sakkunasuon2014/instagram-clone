718738f99eb004f04683b562e79c92c96fefe6da
Sakkuna Suon <sakkunasuon2014@gmail.com>
2026-03-11 18:14:07 +0700

    Add likes table and update post schema to include likes relationship

--- apps/backend/src/posts/schemas/schema.ts
+++ apps/backend/src/posts/schemas/schema.ts
@@ -17,9 +17,31 @@ export const post = pgTable('post', {
     .references(() => user.id),
 });
 
-export const postRelation = relations(post, ({ one }) => ({
+export const postRelation = relations(post, ({ one, many }) => ({
   user: one(user, {
     fields: [post.userId],
     references: [user.id],
+  }),
+  likes: many(like),
+}));
+
+export const like = pgTable('likes', {
+  id: serial('id').primaryKey(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id),
+  postId: integer('post_id')
+    .notNull()
+    .references(() => post.id),
+});
+
+const likeRelations = relations(like, ({ one }) => ({
+  user: one(user, {
+    fields: [like.userId],
+    references: [user.id],
+  }),
+  post: one(post, {
+    fields: [like.postId],
+    references: [post.id],
   }),
 }));
